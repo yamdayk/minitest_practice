27c8f28e6a7c7b36db66901a210ea0650574b0c9
None <y_sun_yoko@yahoo.co.jp>
2016-08-21 05:28:13 +0000

    Implement - calc BMI

--- lib/minitest_practice.rb
+++ lib/minitest_practice.rb
@@ -2,4 +2,6 @@ require "minitest_practice/version"
 
 module MinitestPractice
   # Your code goes here...
+  def self.calcBMI(height,weight)
+                puts "Your BMI is " + (weight/(height*height)).to_s
 end
